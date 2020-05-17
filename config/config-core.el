c322353b09688649ff45c3f29e1f77240808cd93
harshaqq <virtualxi99@gmail.com>
2020-05-18 00:59:54 +0530

    Added full name and mail address to read from global git config

--- config/config-core.el
+++ config/config-core.el
@@ -1,10 +1,13 @@
 (use-package emacs
+  :demand t
   :custom
   (inhibit-splash-screen t)
   (inhibit-startup-message t)
   (create-lockfiles nil)
   (backup-directory-alist `((".*" . ,mine-cache-directory)))
   (auto-save-file-name-transforms `((".*" , mine-cache-directory)))
+  (user-full-name (string-trim (shell-command-to-string "git config --global user.name")))
+  (user-mail-address (string-trim (shell-command-to-string "git config --global user.email")))  
   :config
   (set-language-environment "UTF-8")
   (set-default-coding-systems 'utf-8)
