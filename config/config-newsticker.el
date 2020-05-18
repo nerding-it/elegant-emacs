85a36d7259febeb17fe6e04e75e732bdd8d4cff7
harshaqq <virtualxi99@gmail.com>
2020-05-19 04:04:36 +0530

    Updated newsticker

--- config/config-newsticker.el
+++ config/config-newsticker.el
@@ -1,8 +1,12 @@
 (use-package newsticker
   :custom
   (newsticker-url-list-defaults nil)
+  (newsticker-retrieval-interval 0)
+  (newsticker-automatically-mark-items-as-old nil)
+  (newsticker-frontend 'newsticker-plainview)
   (newsticker-dir (expand-file-name "news/" mine-cache-directory))
   (newsticker-url-list '(
-			 ("reddit-emacs" "https://www.reddit.com/r/emacs.rss" nil nil nil))))
+			 ("reddit-emacs" "https://www.reddit.com/r/emacs.rss" nil nil nil)
+			 ("reddit-unixporn" "https://www.reddit.com/r/unixporn.rss" nil nil nil))))
 
 (provide 'config-newsticker)
