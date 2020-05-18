f175a8201ce2984a15c84183286ba5c6e9548315
harshaqq <virtualxi99@gmail.com>
2020-05-18 13:15:32 +0530

    Added gnus support for gmail

---
+++ config/config-gnus.el
@@ -0,0 +1,26 @@
+(use-package gnus
+  :hook ((gnus-group-mode-hook . gnus-topic-mode)
+	 (message-mode-hook . flyspell-mode))
+  :config
+  (setq-default gnus-select-method '(nntp "news.gwene.org"))
+  (add-to-list 'gnus-secondary-select-methods '(nnimap "gmail"
+						       (nnimap-address "imap.gmail.com")
+						       (nnimap-server-port "imaps")
+						       (nnir-search-engine imap)
+						       (nnimap-stream ssl)))
+  :custom
+  (smtpmail-smtp-server "smtp.gmail.com")
+  (smtpmail-smtp-service 587)
+  (gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
+  (epa-file-cache-passphrase-for-symmetric-encryption t)
+  (gnus-thread-sort-functions '(gnus-thread-sort-by-most-recent-date
+				(not gnus-thread-sort-by-number)))
+
+  (gnus-use-cache t)
+
+  (gnus-summary-thread-gathering-function 'gnus-gather-threads-by-subject)
+  (gnus-thread-hide-subtree t)
+  (gnus-thread-ignore-subject t)
+  (gnus-use-correct-string-widths nil))
+    
+(provide 'config-gnus)
