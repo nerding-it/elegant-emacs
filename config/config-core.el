f3785151511a6c7fc4c9c8d193494b86f3ea04d3
harshaqq <virtualxi99@gmail.com>
2020-05-19 18:49:16 +0530

    Updated mail address outside use package

--- config/config-core.el
+++ config/config-core.el
@@ -1,18 +1,19 @@
 (use-package emacs    	     
   :demand t
-  :requires subr-x
   :custom
   (inhibit-splash-screen t)
   (inhibit-startup-message t)
   (create-lockfiles nil)
   (backup-directory-alist `((".*" . ,mine-cache-directory)))
   (auto-save-file-name-transforms `((".*" , mine-cache-directory)))
-  (user-full-name (string-trim (shell-command-to-string "git config --global user.name")))
-  (user-mail-address (string-trim (shell-command-to-string "git config --global user.email")))
-  (browse-url-browser-function 'eww-browse-url)  
+  (browse-url-browser-function 'eww-browse-url)
   :config
   (set-language-environment "UTF-8")
   (set-default-coding-systems 'utf-8)
   (prefer-coding-system 'utf-8))
+
+(require 'subr-x)
+(setq user-full-name (string-trim (shell-command-to-string "git config --global user.name")))
+(setq user-mail-address (string-trim (shell-command-to-string "git config --global user.email")))
 
 (provide 'config-core)
