5122106bb8947aa9ead2a95f8b914232fac5d797
harshaqq <virtualxi99@gmail.com>
2020-05-19 18:50:38 +0530

    Updated epa file mail address

--- config/config-crypt.el
+++ config/config-crypt.el
@@ -1,10 +1,13 @@
 (use-package epa
+  :after emacs
   :custom
   (epa-file-encrypt-to user-mail-address)
   (epa-pinentry-mode 'loopback)
   (epa-file-select-keys nil))
 
 (use-package org-crypt
-  :after org)
+  :after (org epa)
+  :custom
+  (org-crypt-key epa-file-encrypt-to))
 
 (provide 'config-crypt)
