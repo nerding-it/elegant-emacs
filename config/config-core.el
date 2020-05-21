efa87d84c14b26f042c62d3597188d0e74ffabde
harshaqq <virtualxi99@gmail.com>
2020-05-21 16:14:27 +0530

    Added alias for yes or no

--- config/config-core.el
+++ config/config-core.el
@@ -6,14 +6,15 @@
   (create-lockfiles nil)
   (backup-directory-alist `((".*" . ,mine-cache-directory)))
   (auto-save-file-name-transforms `((".*" , mine-cache-directory)))
-  (browse-url-browser-function 'eww-browse-url)
-  :config
-  (set-language-environment "UTF-8")
-  (set-default-coding-systems 'utf-8)
-  (prefer-coding-system 'utf-8))
+  (browse-url-browser-function 'eww-browse-url))
 
 (require 'subr-x)
 (setq user-full-name (string-trim (shell-command-to-string "git config --global user.name")))
 (setq user-mail-address (string-trim (shell-command-to-string "git config --global user.email")))
+(set-language-environment "UTF-8")
+(set-default-coding-systems 'utf-8)
+(prefer-coding-system 'utf-8)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
 
 (provide 'config-core)
