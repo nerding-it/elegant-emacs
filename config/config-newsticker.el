3a628343f3223d87183885e4cf6705eab525808b
harshaqq <virtualxi99@gmail.com>
2020-05-21 16:17:57 +0530

    Added twitter emacs feed for newsticker

--- config/config-newsticker.el
+++ config/config-newsticker.el
@@ -7,6 +7,9 @@
   (newsticker-dir (expand-file-name "news/" mine-cache-directory))
   (newsticker-url-list '(
 			 ("reddit-emacs" "https://www.reddit.com/r/emacs.rss" nil nil nil)
+			 ("reddit-memes" "https://www.reddit.com/r/memes.rss" nil nil nil)
+			 ("twitter-emacs" "https://twitrss.me/twitter_user_to_rss/?user=emacs" nil nil nil)
 			 ("reddit-unixporn" "https://www.reddit.com/r/unixporn.rss" nil nil nil))))
+(provide 'config-newsticker)
 
 (provide 'config-newsticker)
