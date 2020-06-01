64b2e62ab8037fcbaf8212844f5a2ae5d92a040a
harshaqq <virtualxi99@gmail.com>
2020-06-01 23:35:30 +0530

    Added exec-path-from-shell

--- config/config-misc.el
+++ config/config-misc.el
@@ -3,7 +3,15 @@
 
 (use-package which-key
   :ensure t
+  :custom
+  (which-key-side-window-max-height 0.9)
+  (max-mini-window-height 10)
   :config
   (which-key-mode))
+
+(use-package exec-path-from-shell
+  :ensure t
+  :config
+  (exec-path-from-shell-initialize))
 
 (provide 'config-misc)
