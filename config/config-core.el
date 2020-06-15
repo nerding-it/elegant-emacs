a9ccdc8f0f7b95dc1f102281055eaf4393cfcc3e
sriharsha <bcc@bcc.bcc>
2020-06-16 00:36:18 +0530

    Increased kill ring size, disabled default key for exit

--- config/config-core.el
+++ config/config-core.el
@@ -8,12 +8,24 @@
   (auto-save-file-name-transforms `((".*" , mine-cache-directory)))
   (browse-url-browser-function 'eww-browse-url))
 
+(use-package simple
+  :custom
+  (kill-ring-max (* 4 2048)))
+
+(global-unset-key (kbd "C-x C-c"))
+(defun mine/exit-emacs ()
+  (interactive)
+  (message "You can't quit emacs"))
+(global-set-key (kbd "C-x C-c") 'mine/exit-emacs)
+
 (require 'subr-x)
 (setq user-full-name (string-trim (shell-command-to-string "git config --global user.name")))
 (setq user-mail-address (string-trim (shell-command-to-string "git config --global user.email")))
+
 (set-language-environment "UTF-8")
 (set-default-coding-systems 'utf-8)
 (prefer-coding-system 'utf-8)
+(setq indent-tabs-mode nil)
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
