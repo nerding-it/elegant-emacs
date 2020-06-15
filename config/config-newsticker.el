1e62b65fce29c4486a60ec7a7ecc6f0406621665
sriharsha <bcc@bcc.bcc>
2020-06-16 00:41:20 +0530

    Removed urls from newsticker config, instead using custom.el

--- config/config-newsticker.el
+++ config/config-newsticker.el
@@ -4,12 +4,5 @@
   (newsticker-retrieval-interval 0)
   (newsticker-automatically-mark-items-as-old nil)
   (newsticker-frontend 'newsticker-treeview)
-  (newsticker-dir (expand-file-name "news/" mine-cache-directory))
-  (newsticker-url-list '(
-			 ("reddit-emacs" "https://www.reddit.com/r/emacs.rss" nil nil nil)
-			 ("reddit-memes" "https://www.reddit.com/r/memes.rss" nil nil nil)
-			 ("twitter-emacs" "https://twitrss.me/twitter_user_to_rss/?user=emacs" nil nil nil)
-			 ("reddit-unixporn" "https://www.reddit.com/r/unixporn.rss" nil nil nil))))
-(provide 'config-newsticker)
-
+  (newsticker-dir (expand-file-name "news/" mine-cache-directory)))
 (provide 'config-newsticker)
