69b16b6c4a596bb4af7cf55f736b9aba587ab17f
sriharsha <bcc@bcc.bcc>
2020-06-16 00:39:20 +0530

    Refactored emms for players and streams

--- config/config-emms.el
+++ config/config-emms.el
@@ -1,12 +1,17 @@
 (use-package emms
   :ensure t
   :config
+  (require 'emms-setup)
   (require 'emms-player-simple)
+  (require 'emms-streams)
+  (require 'emms-librefm-stream)
+  (require 'emms-player-mplayer)
   (require 'emms-source-file)
+  (require 'emms-source-playlist)
+  (emms-standard)
+  (emms-default-players)
   :custom
-  (emms-player-list '(emms-player-mpg321
-		      emms-player-ogg123
-		      emms-player-mplayer))
-  (emms-playlist-buffer-name "*Music*"))
-
+  (emms-playlist-buffer-name "*Music*")
+  (emms-player-list '(emms-player-mplayer emms-player-ogg123 emms-player-mpg321)))
+  
 (provide 'config-emms)
