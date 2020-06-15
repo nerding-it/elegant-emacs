9515b14bbdb38ec85e2cb77cbd58ec98e5ffb074
sriharsha <bcc@bcc.bcc>
2020-06-16 00:42:00 +0530

    Added more keybindings for windmove

--- config/config-windows.el
+++ config/config-windows.el
@@ -1,10 +1,20 @@
 (use-package windmove
+  :requires register
+  :bind (("s-h" . windmove-left)
+	 ("s-l" . windmove-right)
+	 ("s-k" . windmove-up)
+	 ("s-j" . windmove-down)
+	 ("s-w" . window-configuration-to-register))
   :config
   (windmove-default-keybindings 'meta))
 
 (use-package winner
+  :custom
+  (winner-ring-size 1024)
+  (winner-boring-buffers '("*Completions*" "*Help" "*Apropos*" "*Buffer List*" "*Compile Log*" "*Calendar*" "*Warnings*"))
   :bind (("<XF86Back>" . winner-undo)
-	 ("<XF86Forward>" . winner-redo))
+	 ("<XF86Forward>" . winner-redo)
+	 ("s-u" . winner-undo))
   :init
   (winner-mode))
 
